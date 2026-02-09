c657a5203af038577fa2b9df0d594ea7727de1bd
Edmond <enzivugira@colgate.edu>
2026-02-08 20:26:42 -0500

    Implemented screen management logic similar to one in the C code

--- LAB_03/starter_code_ball_game/ball_game.py
+++ LAB_03/starter_code_ball_game/ball_game.py
@@ -13,10 +13,10 @@ def update_score(game):
         game.ball = Ball(10, Vector2(400, 210), Vector2(-2.0, -2.5))
     
     
-# class GameState:
-#     MENU = 0
-#     GAMEPLAY = 1
-#     GAME_OVER = 2
+class GameState:
+    MENU = 0
+    GAMEPLAY = 1
+    GAME_OVER = 2
     
     
 class Ball():
@@ -79,23 +79,38 @@ class Enemy_Paddle(Paddle):
 class Game:
 
     def __init__(self):
+        self.state = GameState.MENU
         self.visible = True
         self.moving = False
         self.ball = Ball(10, Vector2(400, 210), Vector2(2.0, 2.5))
         self.player_paddle = Player_Paddle(Vector2(10, 200))
         self.enemy_paddle = Enemy_Paddle(Vector2(WINDOW_WIDTH - 30, 200))
-
-    # where game assets/resources will be initialized
+        
     def startup(self):
         pass
-
+    
     def update(self):
-        self.visible = not is_key_down(
-            KeyboardKey.KEY_SPACE)  # change it to a toogle
-
-        if is_key_pressed(KeyboardKey.KEY_S):  # change it to a toogle
+        if self.state == GameState.MENU:
+            self.update_menu()
+        elif self.state == GameState.GAMEPLAY:
+            self.update_gameplay()
+        elif self.state == GameState.GAME_OVER:
+            self.update_game_over()
+            
+    def update_menu(self):
+        '''Handle menu screen logic'''
+        if is_key_pressed(KeyboardKey.KEY_ENTER):
+            self.state = GameState.GAMEPLAY
+            # self.visible = True
+            self.moving = True
+            
+    def update_gameplay(self):
+        '''Handle gameplay screen logic'''
+        self.visible = not is_key_down(KeyboardKey.KEY_SPACE)  # change it to a toogle
+        
+        if is_key_pressed(KeyboardKey.KEY_S):
             self.moving = not self.moving
-
+            
         if self.visible and self.moving:
             # check for ball and player_paddle collision
             if (self.ball.position.x - self.ball.radius <= self.player_paddle.position.x + self.player_paddle.width):
@@ -109,13 +124,34 @@ class Game:
                     self.ball.position.y <= self.enemy_paddle.position.y + self.enemy_paddle.height):
                     self.ball.velocity.x = self.ball.velocity.x * -1.0
 
-            # , self.screenWidth, 0, self.screenHeight)
+            
             self.ball.update(self.player_paddle)
             update_score(self)
             self.player_paddle.update()
             self.enemy_paddle.update(self.ball)
+            
+        # check for game over condition (first to 10 points wins)
+        if self.player_paddle.score >= 10 or self.enemy_paddle.score >= 10:
+            self.state = GameState.GAME_OVER
 
+    def update_game_over(self):
+        '''Handle game over screen logic'''
+        if is_key_pressed(KeyboardKey.KEY_ENTER):
+            self.__init__()  # Reset the game
+    
     def draw(self):
+        if self.state == GameState.MENU:
+            self.draw_menu()
+        elif self.state == GameState.GAMEPLAY:
+            self.draw_gameplay()
+        elif self.state == GameState.GAME_OVER:
+            self.draw_game_over() 
+            
+    def draw_menu(self):
+        draw_text("PONG!", 150, 150, 40, DARKGREEN)
+        draw_text("Press Enter to Start", 200, 200, 40, DARKGREEN)
+        
+    def draw_gameplay(self):
         draw_fps(20, 20)
 
         if (self.visible):
@@ -131,6 +167,54 @@ class Game:
             self.enemy_paddle.draw()
         else:
             draw_text("Invisible!", 200, 200, 40, RED)
+            
+    def draw_game_over(self):
+        winner = "Player" if self.player_paddle.score >= 10 else "Enemy"
+        draw_text(f"{winner} Wins!", 150, 150, 40, DARKGREEN)
+        draw_text("Press Enter to Restart", 200, 200, 40, DARKGREEN)
+        
+    # def update(self):
+    #     self.visible = not is_key_down(
+    #         KeyboardKey.KEY_SPACE)  # change it to a toogle
+
+    #     if is_key_pressed(KeyboardKey.KEY_S):  # change it to a toogle
+    #         self.moving = not self.moving
+
+    #     if self.visible and self.moving:
+    #         # check for ball and player_paddle collision
+    #         if (self.ball.position.x - self.ball.radius <= self.player_paddle.position.x + self.player_paddle.width):
+    #             if (self.ball.position.y >= self.player_paddle.position.y and
+    #                 self.ball.position.y <= self.player_paddle.position.y + self.player_paddle.height):
+    #                 self.ball.velocity.x = self.ball.velocity.x * -1.0
+                    
+    #         # check for ball and enemy_paddle collision
+    #         if (self.ball.position.x + self.ball.radius >= self.enemy_paddle.position.x):
+    #             if (self.ball.position.y >= self.enemy_paddle.position.y and
+    #                 self.ball.position.y <= self.enemy_paddle.position.y + self.enemy_paddle.height):
+    #                 self.ball.velocity.x = self.ball.velocity.x * -1.0
+
+    #         # , self.screenWidth, 0, self.screenHeight)
+    #         self.ball.update(self.player_paddle)
+    #         update_score(self)
+    #         self.player_paddle.update()
+    #         self.enemy_paddle.update(self.ball)
+
+    # def draw(self):
+    #     draw_fps(20, 20)
+
+    #     if (self.visible):
+    #         # draw score boards
+    #         draw_text(f"Player: {self.player_paddle.score}",
+    #                   120, 20, 20, BLUE)
+    #         draw_text(f"Enemy: {self.enemy_paddle.score}",
+    #                   WINDOW_WIDTH - 150, 20, 20, RED)
+    #         draw_line(WINDOW_WIDTH // 2, 0,
+    #                   WINDOW_WIDTH // 2, WINDOW_HEIGHT, GRAY)
+    #         self.ball.draw()
+    #         self.player_paddle.draw()
+    #         self.enemy_paddle.draw()
+    #     else:
+    #         draw_text("Invisible!", 200, 200, 40, RED)
 
     def shutdown(self):
         pass
