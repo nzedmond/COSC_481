667aec4a323df4713ab553954b80cc5d7c2867bf
Edmond <enzivugira@colgate.edu>
2026-02-18 10:24:52 -0500

    added a c code for the game to translate it into python

--- GAMES/main.py
+++ GAMES/main.py
@@ -9,8 +9,8 @@ current_game = Game()
 
 if __name__ == "__main__":
     init_window(WINDOW_WIDTH, WINDOW_HEIGHT, b"Catch the Falling Objects")
-    set_target_fps(60)
-    
+    set_target_fps(400)
+
     while not window_should_close():
         current_game.update()
         begin_drawing()
