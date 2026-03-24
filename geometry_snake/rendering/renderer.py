459b5766c27f49783ed3593c8bc0073129ad2aec
Edmond <enzivugira@colgate.edu>
2026-03-24 03:20:28 -0400

    drew obstacles

--- geometry_snake/rendering/renderer.py
+++ geometry_snake/rendering/renderer.py
@@ -1,13 +1,15 @@
 from pyray import *
 
 class Renderer:
-    def __init__(self, player, trail):
+    def __init__(self, player, trail, obstacles):
         self.player = player
         self.trail = trail
+        self.obstacles = obstacles
         
     def draw(self):
         self.draw_trail()
         self.draw_player()
+        self.draw_obstacles()
         
     def draw_player(self):
         draw_circle(int(self.player.pos.x), int(self.player.pos.y), 6, WHITE)
@@ -15,3 +17,7 @@ class Renderer:
     def draw_trail(self):
         for point in self.trail.points:
             draw_circle(int(point.x), int(point.y), 3, GRAY)
+            
+    def draw_obstacles(self):
+        for obs in self.obstacles:
+            draw_rectangle(obs.x, obs.y, obs.w, obs.h, RED)
